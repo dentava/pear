1da92b281961db778bbd4513e8ea8f039354d910
Denis Tavaris <32362095+dentava@users.noreply.github.com>
2021-06-21 15:34:04 +0200

    updated home

--- home.py
+++ home.py
@@ -50,3 +50,47 @@ GPIO.setup(bathroom_button, GPIO.IN)
 GPIO.setup(kitchen_button, GPIO.IN)
 GPIO.setup(outside_button, GPIO.IN)
 
+
+def changestate(pin):
+    if GPIO.input(pin) == 1:
+        GPIO.output(pin, 0)
+    else:
+        GPIO.output(pin, 1)
+
+
+while True:
+
+    # Gestione Pulsanti illuminazione
+    if GPIO.input(livingroom_button) == 1:
+        changestate(livingroom)
+
+    if GPIO.input(big_bedroom_button) == 1:
+        changestate(big_bedroom)
+
+    if GPIO.input(small_bedroom_button) == 1:
+        changestate(small_bedroom)
+
+    if GPIO.input(bathroom_button) == 1:
+        changestate(bathroom)
+
+    if GPIO.input(kitchen_button) == 1:
+        changestate(kitchen)
+
+    if GPIO.input(outside_button) == 1:
+        changestate(outside)
+
+    # Gestione Pulsante cancello
+    '''
+    print (GPIO.input(gate_button))
+    if GPIO.input(gate_button) == 1:
+        magic()
+    '''
+
+    # Gestione fotosensore
+    '''
+    if GPIO.input(light) == False:
+        GPIO.output(outside, True)
+
+    if GPIO.input(light) == True:
+        GPIO.output(outside, False)
+    '''
